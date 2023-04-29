b1d92d8ee01e28a5d40711a97acfa7b38f9bd43d
L.B <mrlawrencelane@gmail.com>
2023-04-29 12:18:42 -0400

    checkpoint

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -1,19 +1,39 @@
 <script lang="ts">
-	import type { Post } from '$lib/types';
+  import type { Post } from '$lib/types';
 
-	export let posts: Post[] = [];
+  export let posts: Post[] = [];
 
+  const nestedPosts: { [section: string]: Post[] } = {};
+  posts.forEach(post => {
+    const slugParts = post.slug.split('/');
+    const topLevelSlug = slugParts[0];
+    if (!nestedPosts[topLevelSlug]) {
+      nestedPosts[topLevelSlug] = [post];
+    } else {
+      nestedPosts[topLevelSlug].push(post);
+    }
+  });
+
+  console.log(nestedPosts);
 </script>
 
 <nav class="list-nav p-4">
-	<ul>
-		{#each posts as {slug, title}, i}
-
-			<li >
-				<a href="/{slug}">
-					{title}
-				</a>
-			</li>
-		{/each}
-	</ul>
+  <ul>
+    {#each Object.keys(nestedPosts) as section}
+      <li>
+        <a href="/{section}" class="">
+          {section}
+        </a>
+        <ul>
+          {#each nestedPosts[section].slice(1) as post}
+            <li>
+              <a href="{post.slug}" class="text-sm ml-2">
+                {post.title}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </li>
+    {/each}
+  </ul>
 </nav>
